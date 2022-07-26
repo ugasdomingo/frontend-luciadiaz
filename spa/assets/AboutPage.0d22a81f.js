1d9f3fb5f54eb6deaeee6e000eab6e47883158b0
Domingo Ugas <ugasdomingo@gmail.com>
2022-07-26 12:34:31 -0400

    Updates

---
+++ spa/assets/AboutPage.0d22a81f.js
@@ -0,0 +1,1 @@
+import{_ as e,E as r,O as t}from"./index.73122178.js";const c={};function o(a,n){return r(),t("h3",null," About ")}var s=e(c,[["render",o]]);export{s as default};
---
+++ spa/assets/PsicologiaPage.aa7c2c97.js
@@ -0,0 +1,1 @@
+import{_ as e,E as o,O as c}from"./index.73122178.js";const a={};function r(n,s){return o(),c("h3",null," Psicolog\xEDa Blog ")}var l=e(a,[["render",r]]);export{l as default};
