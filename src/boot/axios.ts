64a1d8e7fe7ac8456c1161c5dd6f883af05379e8
Domingo Ugas <ugasdomingo@gmail.com>
2022-10-24 13:53:55 -0400

    Axios changed

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -2,20 +2,20 @@ import { boot } from 'quasar/wrappers';
 import axios, { AxiosInstance } from 'axios';
 
 declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $axios: AxiosInstance;
-  }
+    interface ComponentCustomProperties {
+        $axios: AxiosInstance;
+    }
 }
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  withCredentials: true,
+    baseURL: 'https://backend-luciadiaz.vercel.app',
+    withCredentials: true,
 });
 
 export default boot(({ app }) => {
-  app.config.globalProperties.$axios = axios;
+    app.config.globalProperties.$axios = axios;
 
-  app.config.globalProperties.$api = api;
+    app.config.globalProperties.$api = api;
 });
 
 export { api };
