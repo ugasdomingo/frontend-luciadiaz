18167fd53a065553f21cb92d911324f58812f6fe
Domingo Ugas <ugasdomingo@gmail.com>
2022-10-24 13:56:30 -0400

    Axios 2

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -8,7 +8,7 @@ declare module '@vue/runtime-core' {
 }
 
 const api = axios.create({
-    baseURL: 'https://backend-luciadiaz.vercel.app',
+    baseURL: 'https://backend-luciadiaz.vercel.app/api',
     withCredentials: true,
 });
 
